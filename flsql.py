7fed7a9519fc27a96dfab18651813ee9f0bc4cdb
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-10-23 01:32:09 +0300

    Update returned function for get post when error appeared

--- flsql.py
+++ flsql.py
@@ -1,23 +1,23 @@
-'''
+"""
 Database sql helper
-'''
+"""
 
 import psycopg2
 import psycopg2.extras
 from datetime import datetime
 import math
 class Flsql:
-    '''
+    """
     Create Flask SQL class for sql query
-    '''
+    """
     def __init__(self, cur_db):
         self.__my_db = cur_db
         self.__cur = cur_db.cursor(cursor_factory = psycopg2.extras.RealDictCursor)
 
     def get_menu(self):
-        '''
+        """
         Function putting main menu links
-        '''
+        """
         sql = '''SELECT * FROM mainmenu ORDER BY ID'''
         try:
             self.__cur.execute(sql)
@@ -30,9 +30,9 @@ class Flsql:
 
 
     def add_post(self, title, text):
-        '''
+        """
         Method for adding post at site
-        '''
+        """
         try:
             cur_time = datetime.now()
             self.__cur.execute('''INSERT INTO posts (title, posttext, posttime) VALUES (%s, %s, %s)''', (title, text, cur_time))
@@ -42,16 +42,17 @@ class Flsql:
             return False
         return True
 
+
     def get_post(self, postId):
-        '''
+        """
         This method get post by id
-        '''
+        """
         try:
-            self.__cur.execute(f"SELECT title, text FROM posts WHERE id = {postId} LIMIT 1")
+            self.__cur.execute(f"SELECT title, posttext FROM posts WHERE id = {postId} LIMIT 1")
             res = self.__cur.fetchone()
             if res:
                 return res
         except psycopg2.OperationalError as err:
             print('Ошибка добавления статьи в БД: ', str(err))
 
-        return (False, False)
+        return {'title': False, 'posttext': False}
