95f86e078b8fad28986b3759325af525c676e4be
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-10-23 15:46:28 +0300

    Add new handler for column url into sql schema

--- flsql.py
+++ flsql.py
@@ -2,7 +2,7 @@
 Database sql helper
 """
 from datetime import datetime
-import psycopg2
+from psycopg2 import OperationalError
 import psycopg2.extras
 
 class Flsql:
@@ -23,21 +23,22 @@ class Flsql:
             res = self.__cur.fetchall()
             if res:
                 return res
-        except psycopg2.OperationalError as err:
-            print('Ошибка чтения из БД', str(err) )
+        except OperationalError as err:
+            print('Ошибка чтения из БД', str(err))
         return []
 
 
-    def add_post(self, title, text):
+    def add_post(self, title, text, url):
         """
         Method for adding post at site
         """
         try:
             cur_time = datetime.now()
-            self.__cur.execute("INSERT INTO posts (title, posttext, posttime) VALUES (%s, %s, %s);",
-                (title, text, cur_time))
+            self.__cur.execute("INSERT INTO posts (title, posttext, posturl, posttime)\
+                                VALUES (%s, %s, %s, %s);",
+                                (title, text, url, cur_time))
             self.__my_db.commit()
-        except psycopg2.OperationalError as err:
+        except OperationalError as err:
             print('Ошибка добавления статьи в БД: ', str(err))
             return False
         return True
@@ -52,7 +53,7 @@ class Flsql:
             res = self.__cur.fetchone()
             if res:
                 return res
-        except psycopg2.OperationalError as err:
+        except OperationalError as err:
             print('Ошибка чтения из БД: ', str(err))
 
         return {'title': False, 'posttext': False}
@@ -67,6 +68,6 @@ class Flsql:
             res = self.__cur.fetchall()
             if res:
                 return res
-        except psycopg2.OperationalError as err:
+        except OperationalError as err:
             print('Ошибка чтения из БД: ', str(err))
         return {}
