4a26cdb86073d8cea1380c575a17d9daca8d4ef9
Ignatev Nikita <nikitaignatiev@gmail.com>
2022-10-28 13:37:42 +0300

    Add user function

--- flsql.py
+++ flsql.py
@@ -19,14 +19,14 @@ class Flsql:
         """
         Function putting main menu links
         """
-        sql = "SELECT * FROM mainmenu ORDER BY ID;"
+        sql = 'SELECT * FROM mainmenu ORDER BY ID;'
         try:
             self.__cur.execute(sql)
             res = self.__cur.fetchall()
             if res:
                 return res
         except OperationalError as err:
-            print("Ошибка чтения из БД", str(err))
+            print('Ошибка чтения из БД', str(err))
         return []
 
 
@@ -36,7 +36,7 @@ class Flsql:
         """
         try:
             self.__cur.execute(
-                f"SELECT COUNT(*) as \"count\" FROM posts WHERE posturl LIKE '{url}';")
+                f'SELECT COUNT(*) as "count" FROM posts WHERE posturl LIKE "{url}";')
             res = self.__cur.fetchone()
             if res["count"] > 0:
                 print("Статья с таким url уже существует.")
@@ -48,12 +48,12 @@ class Flsql:
                 text)
 
             cur_time = datetime.now()
-            self.__cur.execute("INSERT INTO posts (title, posttext, posturl, posttime)\
-                                VALUES (%s, %s, %s, %s);",
+            self.__cur.execute('INSERT INTO posts (title, posttext, posturl, posttime)\
+                                VALUES (%s, %s, %s, %s);',
                                 (title, text, url, cur_time))
             self.__my_db.commit()
         except OperationalError as err:
-            print("Ошибка добавления статьи в БД: ", str(err))
+            print('Ошибка добавления статьи в БД: ', str(err))
             return False
         return True
 
@@ -63,14 +63,14 @@ class Flsql:
         This method get post by id
         """
         try:
-            self.__cur.execute(f"SELECT title, posttext FROM posts WHERE posturl LIKE '{alias}' LIMIT 1;")
+            self.__cur.execute(f'SELECT title, posttext FROM posts WHERE posturl LIKE "{alias}" LIMIT 1;')
             res = self.__cur.fetchone()
             if res:
                 return res
         except OperationalError as err:
-            print("Ошибка чтения из БД: ", str(err))
+            print('Ошибка чтения из БД: ', str(err))
 
-        return {"title": False, "posttext": False}
+        return {'title': False, 'posttext': False}
 
 
     def get_posts_announcement(self):
@@ -78,10 +78,29 @@ class Flsql:
         Return announcement of the posts
         """
         try:
-            self.__cur.execute("SELECT id, title, posttext, posturl FROM posts ORDER BY posttime DESC;")
+            self.__cur.execute('SELECT id, title, posttext, posturl FROM posts ORDER BY posttime DESC;')
             res = self.__cur.fetchall()
             if res:
                 return res
         except OperationalError as err:
-            print("Ошибка чтения из БД: ", str(err))
+            print('Ошибка чтения из БД: ', str(err))
         return {}
+
+
+    def addUser(self, name, email, hpsw):
+        try:
+            self.__cur.execute(
+                f'SELECT COUNT(*) as "count" FROM users WHERE email LIKE "{email}";')
+            res = self.__cur.fetchone()
+            if res['count'] > 0:
+                print('Пользователь с таким email уже существует.')
+                return False
+            cur_time = datetime.now()
+            self.__cur.execute('INSERT INTO users (name, email, psw, date)\
+                                VALUES (%s, %s, %s, %s);',
+                                (name, email, hpsw, cur_time))
+            self.__my_db.commit()
+        except OperationalError as err:
+            print('Ошибка добавления пользователя в БД: ', str(err))
+            return False
+        return True
